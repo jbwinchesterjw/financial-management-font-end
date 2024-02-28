7708b637c9c126e5726da360fb0dcc1c8a937e20
João Batista <jbwincheaster@gmail.com>
2024-02-28 14:06:38 -0300

    Atualizando url da aplicação

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://swift-finance-ba40953e127f.herokuapp.com',
+  apiUrl: 'https://financial-management-back-end-431b86b830b1.herokuapp.com',
   tokenAllowedDomains: [ /financial-management-back-end-431b86b830b1.herokuapp.com/ ],
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
